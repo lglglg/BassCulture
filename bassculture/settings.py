a977e53f9bc3aa7566d8d35c0a732d1c381a04fd
luca.guariento <luca.guariento@glasgow.ac.uk>
2015-06-01 12:48:12 +0100

    no message

--- bassculture/settings.py
+++ bassculture/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR,'/static/')
-STATIC_ROOT = ''
 STATICFILES_DIRS = ( os.path.join(PROJECT_DIR,'static/'),)
-STATIC_PRECOMPILER_ROOT = '/static/'
+#STATIC_PRECOMPILER_ROOT = '/static/'
 
 SOLR_SERVER = 'http://localhost:8080'
