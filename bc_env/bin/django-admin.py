4785b5ac5d10e24e74d7b1ef976e872c5de54d1b
luca.guariento <luca.guariento@glasgow.ac.uk>
2015-06-03 16:30:14 +0100

    ___

--- bc_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/BassCulture/Documents/Code/Git/BassCulture/bc_env/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
